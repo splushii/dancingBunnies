549fd96fe6e9c99d9c7c3e759ad58303d12dede0
Christian Hernvall <christian.hernvall@gmail.com>
2019-01-10 21:38:38 +0100

    Add some tag columns to the song DB.

--- app/src/main/java/se/splushii/dancingbunnies/storage/RoomMetaSong.java
+++ app/src/main/java/se/splushii/dancingbunnies/storage/RoomMetaSong.java
@@ -27,9 +27,9 @@ public class RoomMetaSong {
             Meta.METADATA_KEY_API,
 //        METADATA_KEY_ARTIST_ID,
 //        METADATA_KEY_AVERAGE_RATING,
-//        METADATA_KEY_BITRATE,
+            Meta.METADATA_KEY_BITRATE,
 //        METADATA_KEY_BOOKMARK_POSITION,
-//        METADATA_KEY_CONTENT_TYPE,
+            Meta.METADATA_KEY_CONTENT_TYPE,
 //        METADATA_KEY_DATE_ADDED,
 //        METADATA_KEY_DATE_STARRED,
 //        METADATA_KEY_FILE_SIZE,
@@ -44,7 +44,7 @@ public class RoomMetaSong {
 //        METADATA_KEY_ADVERTISEMENT,
             Meta.METADATA_KEY_ALBUM,
 //        METADATA_KEY_ALBUM_ART,
-//        METADATA_KEY_ALBUM_ARTIST,
+            Meta.METADATA_KEY_ALBUM_ARTIST,
 //        METADATA_KEY_ALBUM_ART_URI,
 //        METADATA_KEY_ART,
             Meta.METADATA_KEY_ARTIST,
@@ -54,24 +54,24 @@ public class RoomMetaSong {
 //        METADATA_KEY_COMPILATION,
 //        METADATA_KEY_COMPOSER,
 //        METADATA_KEY_DATE,
-//        METADATA_KEY_DISC_NUMBER,
+            Meta.METADATA_KEY_DISC_NUMBER,
 //        METADATA_KEY_DISPLAY_DESCRIPTION,
 //        METADATA_KEY_DISPLAY_ICON,
 //        METADATA_KEY_DISPLAY_ICON_URI,
 //        METADATA_KEY_DISPLAY_SUBTITLE,
 //        METADATA_KEY_DISPLAY_TITLE,
 //        METADATA_KEY_DOWNLOAD_STATUS,
-//        METADATA_KEY_DURATION,
-//        METADATA_KEY_GENRE,
+            Meta.METADATA_KEY_DURATION,
+            Meta.METADATA_KEY_GENRE,
             Meta.METADATA_KEY_MEDIA_ID,
 //        METADATA_KEY_MEDIA_URI,
 //        METADATA_KEY_NUM_TRACKS,
 //        METADATA_KEY_RATING,
             Meta.METADATA_KEY_TITLE,
-//        METADATA_KEY_TRACK_NUMBER,
+            Meta.METADATA_KEY_TRACK_NUMBER,
 //        METADATA_KEY_USER_RATING,
 //        METADATA_KEY_WRITER,
-//        METADATA_KEY_YEAR
+            Meta.METADATA_KEY_YEAR
     };
     public static final HashSet DBKeysSet = new HashSet<>(Arrays.asList(db_keys));
 
@@ -81,6 +81,14 @@ public class RoomMetaSong {
     public static final String COLUMN_TITLE = "title";
     public static final String COLUMN_ALBUM = "album";
     public static final String COLUMN_ARTIST = "artist";
+    public static final String COLUMN_ALBUM_ARTIST = "albumartist";
+    public static final String COLUMN_GENRE = "genre";
+    public static final String COLUMN_DURATION = "duration";
+    public static final String COLUMN_CONTENT_TYPE = "contenttype";
+    public static final String COLUMN_YEAR = "year";
+    public static final String COLUMN_TRACK_NUMBER = "tracknum";
+    public static final String COLUMN_TRACK_DISCNUMBER = "discnum";
+    public static final String COLUMN_BITRATE = "bitrate";
 
     @NonNull
     String api;
@@ -90,6 +98,15 @@ public class RoomMetaSong {
     String title;
     String album;
     String artist;
+    String albumartist;
+    String genre;
+    long duration;
+    String contenttype;
+    long year;
+    long tracknum;
+    long discnum;
+    long bitrate;
+
 
     public Meta getMeta() {
         Bundle b = new Bundle();
@@ -99,6 +116,14 @@ public class RoomMetaSong {
         b.putString(Meta.METADATA_KEY_TITLE, title);
         b.putString(Meta.METADATA_KEY_ALBUM, album);
         b.putString(Meta.METADATA_KEY_ARTIST, artist);
+        b.putString(Meta.METADATA_KEY_ALBUM_ARTIST, albumartist);
+        b.putString(Meta.METADATA_KEY_GENRE, genre);
+        b.putLong(Meta.METADATA_KEY_DURATION, duration);
+        b.putString(Meta.METADATA_KEY_CONTENT_TYPE, contenttype);
+        b.putLong(Meta.METADATA_KEY_YEAR, year);
+        b.putLong(Meta.METADATA_KEY_TRACK_NUMBER, tracknum);
+        b.putLong(Meta.METADATA_KEY_DISC_NUMBER, discnum);
+        b.putLong(Meta.METADATA_KEY_BITRATE, bitrate);
         return new Meta(b);
     }
 
@@ -116,6 +141,22 @@ public class RoomMetaSong {
                 return COLUMN_ALBUM;
             case Meta.METADATA_KEY_ARTIST:
                 return COLUMN_ARTIST;
+            case Meta.METADATA_KEY_ALBUM_ARTIST:
+                return COLUMN_ALBUM_ARTIST;
+            case Meta.METADATA_KEY_GENRE:
+                return COLUMN_GENRE;
+            case Meta.METADATA_KEY_DURATION:
+                return COLUMN_DURATION;
+            case Meta.METADATA_KEY_CONTENT_TYPE:
+                return COLUMN_CONTENT_TYPE;
+            case Meta.METADATA_KEY_YEAR:
+                return COLUMN_YEAR;
+            case Meta.METADATA_KEY_TRACK_NUMBER:
+                return COLUMN_TRACK_NUMBER;
+            case Meta.METADATA_KEY_DISC_NUMBER:
+                return COLUMN_TRACK_DISCNUMBER;
+            case Meta.METADATA_KEY_BITRATE:
+                return COLUMN_BITRATE;
             default:
                 return null;
         }
@@ -141,6 +182,30 @@ public class RoomMetaSong {
                     break;
                 case Meta.METADATA_KEY_ARTIST:
                     artist = meta.getString(key);
+                    break;
+                case Meta.METADATA_KEY_ALBUM_ARTIST:
+                    albumartist = meta.getString(key);
+                    break;
+                case Meta.METADATA_KEY_GENRE:
+                    genre = meta.getString(key);
+                    break;
+                case Meta.METADATA_KEY_DURATION:
+                    duration = meta.getLong(key);
+                    break;
+                case Meta.METADATA_KEY_CONTENT_TYPE:
+                    contenttype = meta.getString(key);
+                    break;
+                case Meta.METADATA_KEY_YEAR:
+                    year = meta.getLong(key);
+                    break;
+                case Meta.METADATA_KEY_TRACK_NUMBER:
+                    tracknum = meta.getLong(key);
+                    break;
+                case Meta.METADATA_KEY_DISC_NUMBER:
+                    discnum = meta.getLong(key);
+                    break;
+                case Meta.METADATA_KEY_BITRATE:
+                    bitrate = meta.getLong(key);
                     break;
                 default:
                     Log.e(LC, "Unhandled key: " + key);
